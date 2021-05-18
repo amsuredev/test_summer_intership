1b35d1a9a402ed1af7e6c21f9cb7c94ac5c8d598
Aliaksei Anishchanka <amsure00dev@gmail.com>
2021-05-18 15:30:16 +0200

    feat: first ex

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,7 +9,22 @@ fun main(args: Array<String>) {
     val circles = reader.getMatchedCircle()
     val rectangles = reader.getMatchedRectangle()
     val broken = reader.getMatchedBroken()
-    val a = 5
+    var not_match_indexes = concatenate(circles.second, rectangles.second, broken.second)
+    val comparator = Comparator {str1: String, str2: String -> str1.toInt() - str2.toInt()}
+    not_match_indexes = not_match_indexes.sortedWith(comparator)
+    writeToFile(indexes = not_match_indexes)
+}
+
+fun <T> concatenate(vararg lists: List<T>): List<T> {
+    return listOf(*lists).flatten()
+}
+
+fun writeToFile(file_name:String="exersices/zad1.txt", indexes: List<String>){
+    File(file_name).printWriter().use { out ->
+        indexes.forEach {
+            out.println(it)
+        }
+    }
 }
 
 
